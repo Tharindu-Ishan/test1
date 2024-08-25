3f297617f2aa0e09df9fe816f8b75d5362df3f3c
Tharindu-Ishan <ishanthari96@gmail.com>
2024-08-25 11:31:51 +0530

    Create Service layer

--- src/main/java/com/paymedia/test/project/service/DepartmentService.java
+++ src/main/java/com/paymedia/test/project/service/DepartmentService.java
@@ -1,4 +1,42 @@
 package com.paymedia.test.project.service;
 
+import com.paymedia.test.project.entity.Department;
+import com.paymedia.test.project.repository.DepartmentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Department not found with id " + id));
+    }
+
+    public Department updateDepartment(Long id, Department departmentDetails) {
+        Department department = getDepartmentById(id);
+        department.setName(departmentDetails.getName());
+        return departmentRepository.save(department);
+    }
+
+    public void deleteDepartment(Long id) {
+        Department department = getDepartmentById(id);
+        departmentRepository.delete(department);
+    }
+
+    public Page<Department> getAllDepartments(String search, Pageable pageable) {
+        if (search != null && !search.isEmpty()) {
+            return departmentRepository.findByNameContainingIgnoreCase(search, pageable);
+        }
+        return departmentRepository.findAll(pageable);
+    }
 }
--- src/main/java/com/paymedia/test/project/service/EmployeeService.java
+++ src/main/java/com/paymedia/test/project/service/EmployeeService.java
@@ -1,4 +1,55 @@
 package com.paymedia.test.project.service;
 
+import com.paymedia.test.project.entity.Department;
+import com.paymedia.test.project.entity.Employee;
+import com.paymedia.test.project.repository.DepartmentRepository;
+import com.paymedia.test.project.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Employee not found with id " + id));
+    }
+
+    public Employee updateEmployee(Long id, Employee employeeDetails) {
+        Employee employee = getEmployeeById(id);
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setPhone(employeeDetails.getPhone());
+
+        Department department = departmentRepository.findByName(employeeDetails.getDepartment().getName()).orElseThrow(() -> new EntityNotFoundException("Department not found"));
+        employee.setDepartment(department);
+
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        Employee employee = getEmployeeById(id);
+        employeeRepository.delete(employee);
+    }
+
+    public Page<Employee> getAllEmployees(String departmentName, Pageable pageable) {
+        if (departmentName != null) {
+            Department department = departmentRepository.findByName(departmentName).orElseThrow(() -> new EntityNotFoundException("Department not found"));
+            return employeeRepository.findAllByDepartment(department, pageable);
+        }
+        return employeeRepository.findAll(pageable);
+    }
 }
