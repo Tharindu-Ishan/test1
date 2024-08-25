2d9cb964f2e831ea1dd69523a887cff519cb3892
Tharindu-Ishan <ishanthari96@gmail.com>
2024-08-25 11:17:46 +0530

    Create Entities

--- src/main/java/com/paymedia/test/project/entity/Department.java
+++ src/main/java/com/paymedia/test/project/entity/Department.java
@@ -1,4 +1,28 @@
 package com.paymedia.test.project.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "department")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "departments")
+    private List<Employee> employees = new ArrayList<>();
 }
--- src/main/java/com/paymedia/test/project/entity/Employee.java
+++ src/main/java/com/paymedia/test/project/entity/Employee.java
@@ -1,4 +1,35 @@
 package com.paymedia.test.project.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
