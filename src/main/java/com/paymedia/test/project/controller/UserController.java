67d2a22ff9144e019c4855417987fd071dce3cf9
tharindu ishan <ishanthari96@gmail.com>
2024-08-26 20:44:41 +0530

    Project with spring security

---
+++ src/main/java/com/paymedia/test/project/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.paymedia.test.project.controller;
+
+import com.paymedia.test.project.entity.User;
+import com.paymedia.test.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return service.register(user);
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return service.verify(user);
+    }
+}
---
+++ src/main/java/com/paymedia/test/project/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.paymedia.test.project.service;
+
+import com.paymedia.test.project.entity.User;
+import com.paymedia.test.project.entity.UserPrincipal;
+import com.paymedia.test.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
